47cb51eae0390abc410f881b8b392a0f24b619ea
Bohdan Arkhypchuk <arkhypchuk.b@gmail.com>
2022-04-29 13:33:59 +0300

    Initial Commit

---
+++ Reddit_Comments/Reddit_CommentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reddit_CommentsApp.swift
+//  Reddit_Comments
+//
+//  Created by Bohdan on 29.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Reddit_CommentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
